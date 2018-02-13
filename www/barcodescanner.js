77bbd6340e13655346a84f98ecc527cacee74d20
Gregory Benner <gregbenner1@gmail.com>
2018-02-13 13:21:21 -0500

    remove the error callback for a scan that happens during another scan

--- www/barcodescanner.js
+++ www/barcodescanner.js
@@ -100,7 +100,6 @@ BarcodeScanner.prototype.scan = function (successCallback, errorCallback, config
             }
 
             if (scanInProgress) {
-                errorCallback('Scan is already in progress');
                 return;
             }
 
